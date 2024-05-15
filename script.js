89b2cf05771f0431e88dcbf55916abc303c6d1b1
Vitalii <tather13@gmail.com>
2024-05-15 14:58:35 +0300

    homework_teacher_solution

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 'use strict'
 
-const numberOfFilms = prompt('Сколько фильмов вы уже просмотрели?')
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?');
 
 const personalMovieDB = {
-    count : numberOfFilms,
-    movies:{},
-    actors:{},
-    genres:[],
-    privat : false
-}
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
 let userMovie = prompt('Один из последних просмотренных фильмов?');
-let userGrade = prompt('На скольк оцените его?');
+let userGrade = +prompt('На сколько оцените его?');
+let user1Movie = prompt('Один из последних просмотренных фильмов?');
+let user1Grade = +prompt('На сколько оцените его?');
 
-personalMovieDB['movies'] = `'${userMovie}': '${userGrade}'`
+personalMovieDB.movies[userMovie] = userGrade
+personalMovieDB.movies[user1Movie] = user1Grade
 
-console.log(personalMovieDB.count)
-console.log(personalMovieDB.movies)
+console.log(personalMovieDB)
