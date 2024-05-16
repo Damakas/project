a076bef68147ca677b01d9c9664ba5a3280dc752
Vitalii <tather13@gmail.com>
2024-05-16 12:15:18 +0300

    conditions part

--- script.js
+++ script.js
@@ -19,3 +19,37 @@ personalMovieDB.movies[userMovie] = userGrade
 personalMovieDB.movies[user1Movie] = user1Grade
 
 console.log(personalMovieDB)
+
+if (4 == 9){
+    console.log('ok!')
+} else{
+console.log('Error')
+}
+
+
+if(num < 49){
+    console.log('Error');
+} else if(num >100){
+    console.log('Много');
+}else {
+    console.log('Ok')
+}
+
+(num === 50) ? console.log('Ok!') : console.log('Error')
+
+const num = 50;
+
+switch (num){
+    case 49: 
+        console.log('Неверно');
+        break;  
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('Верно');      
+         break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
