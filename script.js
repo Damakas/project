a395c14fff95fc4187fcce9b812b6d039eb8a6af
Vitalii <tather13@gmail.com>
2024-05-16 14:49:47 +0300

    cycles

--- script.js
+++ script.js
@@ -1,55 +1,22 @@
 'use strict'
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?');
+let num = 50;
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-let userMovie = prompt('Один из последних просмотренных фильмов?');
-let userGrade = +prompt('На сколько оцените его?');
-let user1Movie = prompt('Один из последних просмотренных фильмов?');
-let user1Grade = +prompt('На сколько оцените его?');
-
-personalMovieDB.movies[userMovie] = userGrade
-personalMovieDB.movies[user1Movie] = user1Grade
-
-console.log(personalMovieDB)
-
-if (4 == 9){
-    console.log('ok!')
-} else{
-console.log('Error')
+while(num < 55){
+    console.log(num);
+    num++;
 }
 
-
-if(num < 49){
-    console.log('Error');
-} else if(num >100){
-    console.log('Много');
-}else {
-    console.log('Ok')
+do{
+    console.log(num);
+    num++;
 }
+while(num < 55);
 
-(num === 50) ? console.log('Ok!') : console.log('Error')
-
-const num = 50;
-
-switch (num){
-    case 49: 
-        console.log('Неверно');
-        break;  
-    case 100:
-        console.log('Неверно');
-        break;
-    case 50:
-        console.log('Верно');      
-         break;
-    default:
-        console.log('Не в этот раз');
-        break;
+for (let i = 1; i < 10; i++){
+if (i === 6){
+    break;
+    // continue;
+}
+        console.log(i);
 }
