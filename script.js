2c2e1bf20e52c99c1d5e7036bb46594824aa3797
Vitalii <tather13@gmail.com>
2024-05-16 15:04:00 +0300

    triangle cycle

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 'use strict'
 
-let num = 50;
-
-while(num < 55){
-    console.log(num);
-    num++;
+for(let i = 0; i < 3; i++){
+    console.log(i);
+    for(let j = 0; j < 3; j++){
+        console.log(j)
+    }
 }
 
-do{
-    console.log(num);
-    num++;
-}
-while(num < 55);
+let result = '';
+const length = 7;
 
-for (let i = 1; i < 10; i++){
-if (i === 6){
-    break;
-    // continue;
+for(let i = 1; i < length; i++){
+
+    for(let j = 0; j < i; j++){
+        result += '*'
+    }
+
+    result += '\n'
 }
-        console.log(i);
-}
+
+console.log(result);
