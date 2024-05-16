bc486c93d56087a9b73641663de933316690aebe
Vitalii <tather13@gmail.com>
2024-05-16 16:00:30 +0300

    cycles_n_marks

--- script.js
+++ script.js
@@ -20,3 +20,15 @@ for(let i = 1; i < length; i++){
 }
 
 console.log(result);
+
+
+first:for(let i = 0; i < 3; i++){
+    console.log(`First level :${i}`);
+    for(let j = 0; j < 3; j++){
+        console.log(`Second level :${j}`);
+            for(let k = 0; k < 3; k++){
+                if (k === 2) break first;
+            console.log(`Third level :${k}`);
+        }
+    }
+}
