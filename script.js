1c5382d15f4585cd8efcca45c27e2151b528c259
Vitalii <tather13@gmail.com>
2024-05-17 11:08:23 +0300

    exercises_code4

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
 'use strict'
 
-for (let i = 5; i <= 10; i++){
-    console.log(i)
-}9
 
-for (let i = 20; i > 10; i--){
-    if(i === 13){
-    break;
-}
-    console.log(i)
-}
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const result = [];
 
-for (let i = 2; i <= 10; i++){
-    if(i % 2 === 0){
-        console.log(i)
+for(let i = 0; i < arr.length; i++){
+       result[i] = arr[i];
+}
+console.log(result)
+
+
+
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+
+for(let i = 0; i < data.length; i++){
+    if(typeof(data[i]) === 'number'){
+        data[i] = data[i] * 2;
+    }else if(typeof(data[i]) === 'string'){
+        data[i] = `${data[i]} - done`;
     }
-}
+    }
+    console.log(data)
 
 
-let i = 2;
 
-while(i < 16){
-i++
-if(i % 2 === 1){
-    console.log(i)
-}
-}
+    
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+    for (let i = 1; i <= data.length; i++) {
+    result[i - 1] = data[data.length - i] ; 
+    }
+
+    console.log(result);
 
 
-let arr =[];
-
-for(let i = 5; i <= 10; i++){
-    arr[i - 5] = i;
-}
-console.log(arr)
-
-console.log(arr[0])
