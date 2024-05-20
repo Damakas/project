56828eee0e56f93f867c7b5788650d55c2357e6b
Vitalii <tather13@gmail.com>
2024-05-20 14:10:23 +0300

    functions_arguments_DRY

--- script.js
+++ script.js
@@ -1,35 +1,12 @@
 'use strict'
 
-let num = 20;
+const usdCurr = 28;
+const eurCurr = 32;
 
-function showFirstMessage(text){
-   console.log(text)
-   num = 10;
-};
 
-showFirstMessage('Hello world');
-console.log(num);
-
-function calc(a,b){
-   return a + b;
-};
-
-console.log(calc(4,3));
-console.log(calc(5,6));
-console.log(calc(10,6));
-
-function ret(){
-   let num = 50;
-   return num;
+function convert(amount,curr){
+   console.log(curr * amount)
 }
 
-const anotherNum = ret();
-console.log(anotherNum) 
-
-const logger = function(){
-console.log('Hello');
-};
-
-logger();
-
-const calc = (a,b) => a + b;
+convert(500,usdCurr);
+convert(500,eurCurr);
