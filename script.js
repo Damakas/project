56616453bc1ecd33fe6cf1317e0b2be1696cd7c1
Vitalii <tather13@gmail.com>
2024-05-20 14:02:54 +0300

    functions

--- script.js
+++ script.js
@@ -1,82 +1,35 @@
 'use strict'
 
-/* Задание на урок:
+let num = 20;
 
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
-const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?',"");
-
-
-const personalMovieDB = {
-   count: numberOfFilms,
-   movies: {},
-   actors: {},
-   genres:{},
-   privat: false
+function showFirstMessage(text){
+   console.log(text)
+   num = 10;
 };
 
-// Первый вариант решения с циклом for;
+showFirstMessage('Hello world');
+console.log(num);
 
-// for(let i = 0; i < 2; i++){
-//    const a = prompt('Один из последних фильмов?',""),
-//          b = prompt('На сколько оцените его?',"");
+function calc(a,b){
+   return a + b;
+};
 
-//          if( a != null && b != null && a != '' && b != '' && a.length < 50){
-//             personalMovieDB.movies[a] = b;
-//          } else{
-//             i--;
-//          }
-//       }
+console.log(calc(4,3));
+console.log(calc(5,6));
+console.log(calc(10,6));
 
-// Второй вариант решения с циклом while;
+function ret(){
+   let num = 50;
+   return num;
+}
 
-// let i = 0;
+const anotherNum = ret();
+console.log(anotherNum) 
 
-// while(i < 2){
-//    i++;
-//    const a = prompt('Один из последних фильмов?',""),
-//        b = prompt('На сколько оцените его?',"");
-//    if( a != null && b != null && a != '' && b != '' && a.length < 50
-//        ){
-//       personalMovieDB.movies[a] = b;
-//        }else(i--);
-// }
+const logger = function(){
+console.log('Hello');
+};
 
-// Третий вариант решения с циклом do while;
+logger();
 
-let i = 0;
-
-do{
-   i++;
-      const a = prompt('Один из последних фильмов?',"");
-      const b = prompt('На сколько оцените его?',"");
-      if(a != null && b != null && a != '' && b != '' && a.length < 50){
-         personalMovieDB.movies[a] = b;
-      }else{
-         i--;
-      }
-}while(i < 2);
-
-
-
-         if(personalMovieDB.count < 10){
-            console.log("Просмотренно довольно мало фильмов");
-         } else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30 )
-         { console.log("Вы классический зритель"); 
-         }else if( personalMovieDB.count >= 30){
-            console.log('Вы киноман!');
-         }else{
-            console.log('Произошла ошибка');
-         }
-
-               console.log(personalMovieDB);
+const calc = (a,b) => a + b;
