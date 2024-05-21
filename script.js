1f99d2dba74c1ddd17832e39159052c55a437fff
Vitalii <tather13@gmail.com>
2024-05-21 10:53:45 +0300

    additional_exercises_function2_3

--- script.js
+++ script.js
@@ -1,26 +1,56 @@
 'use strict'
 
-Переписать функцию в два варианта.С использованием тернарного оператора "?" и оператора или "||"
+//Напишите фукнцию min(a,b),которая возвращает наименьшее из чисел a и b.
 
-function checkAge(age){
-   if(age > 18){
-      return true
+function min(a,b){
+   if(a < b){
+      return a
    }else{
-      return confirm('Родители разрешили?')
+      return b
+   }
+};
+
+console.log(min(8,4))
+
+
+//Напишите функцию pow(x,n), которая возводит x в степень n и возвращает результат.
+//Создайте страницу, которая запрашивает x и n, а затем выводит результат pow(x,n).
+// P.S. В этой задаче функция обязана поддерживать только натуральные значения n, т.е. целые от 1 и выше.
+
+
+// Моё перворе решение
+
+function pow(x,n){
+   x = +prompt("Укажите первое число");
+   n = +prompt('Укажите второе число');
+   let sum = x ** n
+  if(x && n >= 1){
+   return sum;
+   } else{
+     return console.log('Только натуральные числа')
    }
 }
-
-function checkAge(age){
-return (age > 18) ? true : confirm ('Родители разрешили?') ;
-}
-console.log(checkAge(15))
+alert(pow())
 
 
-function checkAge(age){
-  return (age >= 18 || confirm('Родители разрешили?'))
-}
-alert(checkAge(18))
+// Решение по учебнику
 
+function pow(x,n){
+let result = x;
 
+   for (let i = 1; i < n; i++){
+      result *= x;
+   }
+   return result;
+};
+
+let x = prompt('x?','');
+let n = prompt('n?','');
+
+if(n >= 1 && n % 1 == 0){
+alert( pow(x, n) );
+}else{
+   alert(`Степень ${n} не поддерживается, используйте натуральное число`)
+};
 
 
