d4230e5a0d988c43da900da9003be6f50a0b05d1
Vitalii <tather13@gmail.com>
2024-05-21 10:11:31 +0300

    additional_exercises_function1

--- script.js
+++ script.js
@@ -1,39 +1,26 @@
 'use strict'
 
-function nameUser(name){
-   return console.log('Привет, ' + name)
-}
-nameUser('Жора');
+Переписать функцию в два варианта.С использованием тернарного оператора "?" и оператора или "||"
 
-
-function returnNeighboringNumbers(num) {
-    return [num - 1, num, num + 1];
-}
-
-console.log(returnNeighboringNumbers(5))
-
-
-
-function getMathResult(num,times) {
-if(typeof(times) !== 'number' || times <= 0){
-   return num;
-}
-
-let str = '';
-
-for(let i = 1; i <= times; i++){
-   if( i === times){
-      str += `${num * i}`;
+function checkAge(age){
+   if(age > 18){
+      return true
    }else{
-      str += `${num * i}---`;
+      return confirm('Родители разрешили?')
    }
 }
 
-return str;
+function checkAge(age){
+return (age > 18) ? true : confirm ('Родители разрешили?') ;
 }
+console.log(checkAge(15))
 
-getMathResult(5,10)
-console.log(getMathResult(1,10))
+
+function checkAge(age){
+  return (age >= 18 || confirm('Родители разрешили?'))
+}
+alert(checkAge(18))
+
 
 
 
