bfda7aee692a1197d13d6921fae59fd3d588bdb3
Vitalii <tather13@gmail.com>
2024-05-21 12:37:22 +0300

    methods_properties

--- script.js
+++ script.js
@@ -1,56 +1,25 @@
 'use strict'
 
-//Напишите фукнцию min(a,b),которая возвращает наименьшее из чисел a и b.
+const str = 'teSt';
+const arr =[1, 2, 3];
 
-function min(a,b){
-   if(a < b){
-      return a
-   }else{
-      return b
-   }
-};
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+console.log(str);
 
-console.log(min(8,4))
+const fruit = "Some fruit";
 
+console.log(fruit.indexOf('fruit'))
 
-//Напишите функцию pow(x,n), которая возводит x в степень n и возвращает результат.
-//Создайте страницу, которая запрашивает x и n, а затем выводит результат pow(x,n).
-// P.S. В этой задаче функция обязана поддерживать только натуральные значения n, т.е. целые от 1 и выше.
+const logg = 'Hello world';
 
+console.log(logg.slice(6));
 
-// Моё перворе решение
+console.log(logg.substring(11,6));
 
-function pow(x,n){
-   x = +prompt("Укажите первое число");
-   n = +prompt('Укажите второе число');
-   let sum = x ** n
-  if(x && n >= 1){
-   return sum;
-   } else{
-     return console.log('Только натуральные числа')
-   }
-}
-alert(pow())
+const num = 12.2;
+console.log(Math.round(num));
 
-
-// Решение по учебнику
-
-function pow(x,n){
-let result = x;
-
-   for (let i = 1; i < n; i++){
-      result *= x;
-   }
-   return result;
-};
-
-let x = prompt('x?','');
-let n = prompt('n?','');
-
-if(n >= 1 && n % 1 == 0){
-alert( pow(x, n) );
-}else{
-   alert(`Степень ${n} не поддерживается, используйте натуральное число`)
-};
-
-
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
