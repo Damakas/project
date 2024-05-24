f209f41f7670b4df87e28077ab5e9db78193d327
Vitalii <tather13@gmail.com>
2024-05-24 11:47:46 +0300

    objects_destructuring

--- script.js
+++ script.js
@@ -1,26 +1,52 @@
 'use strict'
 
-function first(){
-  // Do something
-  setTimeout(function(){
-    console.log(1);
-  },500)
+const options = {
+name: 'test',
+width: 1024,
+height: 1024,
+colors: {
+  border: 'black',
+  bg: 'red'
+},
+makeTest: function(){
+  console.log('Test')
 }
+};
 
-function second(){
-  console.log(2)
+options.makeTest();
+
+//Декструктуризация объекта
+const{border, bg} = options.colors;
+console.log(border)
+
+
+//Преобразуем объект в массив с помощью метоба Object.keys() и подсчитывает количество ключей в объекте с помощью свойства length
+console.log(Object.keys(options).length)
+
+//Как обратится к объекту внутри объекта и вернуть значение
+console.log(options['colors']['border'])
+
+//Оператора delete удаляет элемент из объекта
+
+delete options.name;
+console.log(options);
+
+
+//Перебираем объкет с помощью цикла for in
+//В переменной counter подсчитываем количество ключей в объекте
+
+let counter = 0;
+
+for(let key in options){
+  if(typeof(options[key]) === 'object'){
+    for(let i in options[key]){
+      console.log(`Свойство ${i} имеет значение ${options[key][i]} `)
+      counter++;
+    }
+  }else{
+console.log(`Свойство ${key} имеет значение ${options[key]} `);
+counter++;
+  }
+  
 }
-
-first();
-second();
-
-function learnJS(lang,callback){
-  console.log(`Я учу: ${lang}`)
-  callback();
-}
-
-function done(){
-  console.log('Я прошел этот урок!')
-}
-
-learnJS('Javascript', done);
+console.log(counter)
