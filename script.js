b3492f950736d08833d44991d3d2451affbbefe0
Vitalii <tather13@gmail.com>
2024-05-24 17:10:10 +0300

    copyAndCloneObjects_n_operatorSpread

--- script.js
+++ script.js
@@ -1,47 +1,125 @@
 'use strict'
 
 
+//Примитивы с помощью переменных передют друг друу значения которые независимы друг от друга
+let a = 5,
+    b = a;
 
-//Метод сортировки sort() и пример как сортировать элементы в виде чисел массива 
-const arr = [ 2, 13, 26, 8, 10];
-arr.sort(compareNum);
-console.log(arr)
+  b = b + 5;
 
-function compareNum(a, b){
-  return a - b;
-}
+  console.log(b);
+  console.log(a)
 
-//Как работает свойства length с массивом
-arr[99] = 0;
-console.log(arr.length)
-console.log(arr)
+  const obj = {
+    a: 5,
+    b: 1,
+  };
+
+  //А объекты,массивы,функции не могут копираовать друг друга с помощью тех же переменных,они передют ссылки,если изменить переменную на которую ты передал объект ,изменится и источник 
+  const copy = obj;// Передет ссылку
+
+  copy.a = 10;
+
+  console.log(copy);
+  console.log(obj);
+
+//Как с помощью функции копировать объект
+  function copy(mainObj){
+  let objCopy = {};
+
+  let key;
+  for(key in mainObj){
+    objCopy[key] = mainObj[key]
+  }
+
+  return objCopy;
+  }
+
+  const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+      x: 7,
+      y: 4
+    }
+  };
+
+  const newNumbers = copy(numbers);
+
+  newNumbers.a = 10;
+  
+
+  console.log(newNumbers);
+  console.log(numbers);
+
+  //Как с помощью метода Object.assigh() соединять два и более объктов  в однин
+
+  const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+      x: 7,
+      y: 4
+    }
+  };
+
+  const add = {
+    d: 17,
+    e: 20,
+  };
+
+  // console.log(Object.assign(numbers,add));
 
 
-//Метод forEach позволяет перебирать элементы массива и изменять их с помощью функции обратного вызова
-arr.forEach(function(item, i, arr){
-console.log(`${i}: ${item} внутри массива ${arr}`)
-})
+  // Как с помощь метода Object.assign() клонировать объект
+ const clone = Object.assign({}, add);
 
-//Удаляет последний элемент из массива
-arr.pop();
+ clone.d = 20;
+
+ console.log(add);
+ console.log(clone);
+
+ //Как с помощью метода slice() сделать поверхностную копию массива
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
 
 
-//Добавляет элемент в конец массива
-// arr.push(10);
-// console.log(arr) //Выведет [1, 2, 3, 6, 8, 10]
+newArray[1] = 'dadsadas'
+console.log(newArray);
+console.log(oldArray);
 
-//Первый способ перебора элементов массива
-for(let i = 0; i < arr.length; i++){
-  console.log(arr[i]);
-}
 
-//Второй способ перебора элемнтов массива с помощью цикла for of
-for(let value of arr) {
-console.log(value)
-}
+//Оператор spred '...' передает все элементы из массивов video и blogs в массив internet
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
 
-//Пример работы с методом split() который разделяет строки на подстроки и метод join() который объеденяет элементы массива с строку
-const str = prompt('', '');
-const products = str.split(', ');
-products.sort();
-console.log(products.join('; '));
+      console.log(internet)
+
+
+    // Оператор spread (...) передает элементы из массива num в аргументы функции log
+      function log(a, b , c){
+        console.log(a);
+        console.log(b);
+        console.log(c);
+      }
+
+      const num = [2, 5, 7];
+
+      log(...num)
+
+      //Как с помощь оператора spread '...' создать копию массива
+      const array = ['a', 'b'];
+      const newAarray = [...array];
+      console.log(newAarray)
+
+      //Как с помощью оператора spread '...' создать копию объекта
+      const q = {
+        one: 1,
+        two: 2
+      };
+      const newObj = {...q};
+      console.log(newObj)
+
+
+   
