34af10f350cf424b0b7fbe1f821e8949349e790c
Vitalii <tather13@gmail.com>
2024-05-24 10:44:25 +0300

    Callback_functions

--- script.js
+++ script.js
@@ -1,74 +1,26 @@
 'use strict'
 
-let numbersOfFilms;
-
-function start(){
- numbersOfFilms = +prompt('Сколько фильмов вы уже просмотрели?',"");
-
- while (numbersOfFilms == '' || numbersOfFilms == null || isNaN(numbersOfFilms)){
-      numbersOfFilms = +prompt('Сколько фильмов вы уже просмотрели?',"");
- }
+function first(){
+  // Do something
+  setTimeout(function(){
+    console.log(1);
+  },500)
 }
 
-start();
-
-const personalMovieDB = {
-       count: numbersOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-
-writeYourGenres();
-
-
-   function rememberMyFilms(){
-for(let i = 0; i < 2; i++){
-   const a = prompt('Один из последних просмотренных фильмов?',"").trim(),
-         b = prompt('На сколько оцените его?',"");
-   
-         if(a != null && b != null && a != '' && b != '' && a.length < 50){
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-         }else{
-            console.log('error');
-            i--;
-         }
-   }
-   }
-
-   rememberMyFilms();
-
-
-function detectPersonalLevel(){
-if(personalMovieDB.count < 10){
-   console.log('Просмотренно довольно мало фильмов');
-}else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-   console.log('Вы классический зритель')
-}else if (personalMovieDB.count >= 30){
-   console.log('Вы киноман');
-}else {
-   console.log("Произошла ошибка")
+function second(){
+  console.log(2)
 }
 
+first();
+second();
+
+function learnJS(lang,callback){
+  console.log(`Я учу: ${lang}`)
+  callback();
 }
 
-detectPersonalLevel();
-
-function showMyDB(hidden){
-  if(!hidden){
-console.log(personalMovieDB);
-  }
+function done(){
+  console.log('Я прошел этот урок!')
 }
 
-showMyDB(personalMovieDB.privat)
-
-function writeYourGenres(){
-   for(let i = 1; i <= 3; i++){
-         personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-   }
-}
-
-writeYourGenres();
+learnJS('Javascript', done);
