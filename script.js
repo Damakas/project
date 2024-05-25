ea393f7692cb933c966167124256201aacfa2a5d
Vitalii <tather13@gmail.com>
2024-05-25 12:23:21 +0300

    exercises_code10

--- script.js
+++ script.js
@@ -1,125 +1,68 @@
 'use strict'
 
+const personalPlanPeter = {
+    name: "Peter",
+    age: "29",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
 
-//Примитивы с помощью переменных передют друг друу значения которые независимы друг от друга
-let a = 5,
-    b = a;
-
-  b = b + 5;
-
-  console.log(b);
-  console.log(a)
-
-  const obj = {
-    a: 5,
-    b: 1,
-  };
-
-  //А объекты,массивы,функции не могут копираовать друг друга с помощью тех же переменных,они передют ссылки,если изменить переменную на которую ты передал объект ,изменится и источник 
-  const copy = obj;// Передет ссылку
-
-  copy.a = 10;
-
-  console.log(copy);
-  console.log(obj);
-
-//Как с помощью функции копировать объект
-  function copy(mainObj){
-  let objCopy = {};
-
-  let key;
-  for(key in mainObj){
-    objCopy[key] = mainObj[key]
-  }
-
-  return objCopy;
-  }
-
-  const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-      x: 7,
-      y: 4
+    //Решения мастера
+    showAgeAndLangs: function(plan){
+        const {age} = plan;
+        const {languages} = plan.skills;
+        let str = `Мне ${age} и я владею языками `;
+        languages.forEach(function(lang){
+            str +=`${lang.toUpperCase()}`;
+        });
+        return str;
     }
-  };
+    // Мое решение
+    // showAgeAndLangs: function(personalPlanPeter){
+    //     let result = '';
+    //     const {skills} = personalPlanPeter;
+    //     const {languages} = skills;
+    //    languages.forEach((element,index,array) =>{
+    //     array[index] = element.toUpperCase();
+    //    })
+    //         result +=`Мне ${personalPlanPeter.age} и я владею языками ${languages}`
+    //        return result;    
+    // }     
 
-  const newNumbers = copy(numbers);
+    
+};
 
-  newNumbers.a = 10;
-  
-
-  console.log(newNumbers);
-  console.log(numbers);
-
-  //Как с помощью метода Object.assigh() соединять два и более объктов  в однин
-
-  const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-      x: 7,
-      y: 4
-    }
-  };
-
-  const add = {
-    d: 17,
-    e: 20,
-  };
-
-  // console.log(Object.assign(numbers,add));
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
 
 
-  // Как с помощь метода Object.assign() клонировать объект
- const clone = Object.assign({}, add);
 
- clone.d = 20;
-
- console.log(add);
- console.log(clone);
-
- //Как с помощью метода slice() сделать поверхностную копию массива
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
+//Напишите функцию showExperience, которая будет принимать в себя объект со всеми данными и возвращать строку с опытом.
+function showExperience(personalPlanPeter) {
+const {skills} = personalPlanPeter;
+const {exp} = skills;
+return exp;
+}
+console.log(showExperience(personalPlanPeter));
 
 
-newArray[1] = 'dadsadas'
-console.log(newArray);
-console.log(oldArray);
+// Напишите функцию showProgrammingLangs, которая будет принимать в себя объект со всеми данными и возвращать строку в нужном виде.
+//Пример:
+// showProgrammingLangs(personalPlanPeter)  => "Язык js изучен на 20% Язык php изучен на 10%"
+// Причем функция должна работать вне зависимости от количества языков. Если ни один не указан, то возвращается пустая строка.
+function showProgrammingLangs(personalPlanPeter) {
+const {skills} = personalPlanPeter;
+const {programmingLangs} = skills;
+let result = '';
+for(let key in programmingLangs){
+     result += `Язык ${key} изучен на ${programmingLangs[key]}\n`;
+}
+return result;
+}
+console.log(showProgrammingLangs(personalPlanPeter));
 
 
-//Оператор spred '...' передает все элементы из массивов video и blogs в массив internet
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs = ['wordpress', 'livejournal', 'blogger'],
-      internet = [...video, ...blogs, 'vk', 'facebook'];
-
-      console.log(internet)
-
-
-    // Оператор spread (...) передает элементы из массива num в аргументы функции log
-      function log(a, b , c){
-        console.log(a);
-        console.log(b);
-        console.log(c);
-      }
-
-      const num = [2, 5, 7];
-
-      log(...num)
-
-      //Как с помощь оператора spread '...' создать копию массива
-      const array = ['a', 'b'];
-      const newAarray = [...array];
-      console.log(newAarray)
-
-      //Как с помощью оператора spread '...' создать копию объекта
-      const q = {
-        one: 1,
-        two: 2
-      };
-      const newObj = {...q};
-      console.log(newObj)
-
-
-   
