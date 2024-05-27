2d2f0a59a5c187927d578efc42ce26b1ead03cd0
Vitalii <tather13@gmail.com>
2024-05-27 15:53:53 +0300

    practice_part4

--- script.js
+++ script.js
@@ -1,22 +1,93 @@
 'use strict'
+/* Задание на урок:
 
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
 
-const soldier = {
-health: 400,
-armor: 100,
-sayHello : function(){
-    console.log('Hello')
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+
+const personalMovieDB = {
+       count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: true,
+    start:() => {
+personalMovieDB.count = +prompt('Сколько фильмов вы уже просмотрели?',"");
+
+ while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)){
+      personalMovieDB.count = +prompt('Сколько фильмов вы уже просмотрели?',"");
+ }
+    },
+    detectPersonalLevel: function(){
+if(personalMovieDB.count < 10){
+   console.log('Просмотренно довольно мало фильмов');
+}else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+   console.log('Вы классический зритель')
+}else if (personalMovieDB.count >= 30){
+   console.log('Вы киноман');
+}else {
+   console.log("Произошла ошибка")
 }
+    },
+    rememberMyFilms: function(){
+for(let i = 0; i < 2; i++){
+   const a = prompt('Один из последних просмотренных фильмов?',"").trim(),
+         b = prompt('На сколько оцените его?',"");
+   
+         if(a != null && b != null && a != '' && b != '' && a.length < 50){
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+         }else{
+            console.log('error');
+            i--;
+         }
+   }
+    },
+    toggleVisibleMyDB: function(){
+        if(personalMovieDB.privat){
+            personalMovieDB.privat = true;
+        }else{
+            personalMovieDB.privat = false;
+        }
+    },
+    showMyDB : function(hidden){
+ if(!hidden){
+console.log(personalMovieDB);
+  }
+    },
+writeYourGenres: function(){
+  for(let i = 1; i <= 3; i++){
+    let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+   if(genre == '' || genre == null){
+    console.log('Вы ввели некорректные данные или не ввели их вообще');
+    i--;
+   }else{
+    personalMovieDB.genres[i - 1] = genre;
+   }
+    }
+
+    personalMovieDB.genres.forEach((item,i) => {
+        console.log(`Любимый жанр ${i +1} - это ${item}`)
+    })
+   
+},
 };
-//Создание объекта john с прототипом в качестве  soldier
-const john = Object.create(soldier);
+personalMovieDB.start();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.showMyDB(personalMovieDB.privat)
+personalMovieDB.writeYourGenres();
 
-//Присвоение объекту john прототип в качестве soldier
-const john = {
-    health: 100,
-};
-john.__proto__ = soldier;
 
-Object.setPrototypeOf(john, soldier)
 
-john.sayHello();
+
+
+
