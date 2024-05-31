71eb3d646797e521421f01d20fbb519b0ac4f922
Vitalii <tather13@gmail.com>
2024-05-31 15:05:07 +0300

    exercises_code16_factorial

--- script.js
+++ script.js
@@ -1,113 +1,22 @@
 'use strict'
 
-// function pow(x, n){
-//   let result = 1;
+// Напишите функцию, которая вычисляет факториал.
+
+// Задание простое, но нужно понимать что такое факториал вообще. Факториал  – это число, умноженное на "себя минус один", затем на "себя минус два", и так далее до 1. Факториал n обозначается как n!
+
+// Отсюда мы можем понять, что функция должна принимать 1 аргумент, который будет являться числом. 
+// Будет неплохо, если вы на собеседовании сразу напишите проверку на приходящее значение :)
+// Поэтому, если в нашу функцию приходит дробное число или не число  - возвращается строка с любым сообщением на ваше усмотрение.
+// Если 0 и меньше - возвращается число 1.
 
 
-//   for(let i = 0; i < n; i++){
-//     result *= x;
-//   }
-
-//   return result;
-// }
-
-// function pow(x, n){ 
-//   if( n === 1) { 
-//     return x; 
-//   }else {
-//     return x * pow(x, n-1); 
-//   }
-// }
-
-// pow(2,1) // 2 
-// pow(2,2) // 4
-// console.log(pow(2,3)); // 8
-// console.log(pow(2,4)) // 16
-
-
-//Задча,подсчитать общее среднее значения прогресса всех студентов  
-//Для этого нужно количество студентов разделить на общую сумму прогресса
-
-let students = {
-  js : [{
-    name: 'John',
-    progress: 100
-  },{
-    name: 'Ivan',
-    progress: 60
-  }],
-
-  html: {
-    basic: [{
-    name: 'Peter',
-    progress: 20
-  },{
-    name: 'Ann',
-    progress: 18
-  }],
-  
-pro: [{
-  name: 'Sam',
-  progress: 10
-}]
+function factorial(n) {
+  if(typeof(n) != 'number' || !Number.isInteger(n)){
+    return 'Ошибка'
+  }else if(n === 1 || n <= 0) {
+    return 1;
+  }else{
+    return n * factorial(n - 1);
   }
-};
-
-
-// Решение задачи без рекурсии 
-
-// function getTotalProgressByIteration(data) {
-//   let total = 0;
-//   let students = 0;
-
-//   for(let course of Object.values(data)) {
-//     if(Array.isArray(course)) {
-//         students += course.length;
-
-//         for(let i = 0; i < course.length; i++){
-//           total += course[i].progress;
-//         }
-//     }else{
-//      for (let subCourse of Object.values(course)){
-//         students += subCourse.length;
-//           for(let i = 0; i < subCourse.length; i++){
-//           total += subCourse[i].progress;
-//         }
-//      }
-//     }
-//   }
-
-//   return total / students;
-// }
-
-// console.log(getTotalProgressByIteration(students))
-
-
-//Решение задачи с помощью рекурсии
-
-function getTotalProgressByRecursion(data){
-  if (Array.isArray(data)){
-    let total = 0;
-  
-
-  for(let i = 0; i < data.length; i++){
-    total += data[i].progress;
-   }
-
-   return [total, data.length];
- } else {
-  let total = [0, 0];
-
-  for(let subData of Object.values(data)){
-    const subDataArr = getTotalProgressByRecursion(subData);
-    total[0] += subDataArr[0];
-    total[1] += subDataArr[1];
-  }
-
-  return total;
- }
 }
-
-const result = getTotalProgressByRecursion(students);
-
-console.log(result[0]/result[1]);
+console.log(factorial(3));
