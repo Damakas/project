84c5ab8fcd82352d12d87509597cb6f8d262f987
Vitalii <tather13@gmail.com>
2024-06-08 12:54:12 +0300

    MutationObserver

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 'use strict';
 
+const box = document.querySelector('.box');
 
+// Условия для отслеживание
+let observer = new MutationObserver(mutationRecords => {
+    console.log(mutationRecords);
+});
+
+// Отслеживание нужного эелемента
+observer.observe(box, {
+    childList: true
+});
+
+// Отключение от отлслеживания 
+observer.disconnect();
 
