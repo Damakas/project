5f5974ba3056c878b0bf51f165fe52ee4c512328
Vitalii <tather13@gmail.com>
2024-06-09 09:36:48 +0300

    functionConstructor

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 'use strict';
 
-const box = document.querySelector('.box');
 
-// Условия для отслеживание
-let observer = new MutationObserver(mutationRecords => {
-    console.log(mutationRecords);
-});
+// Функция конструктор
 
-// Отслеживание нужного эелемента
-observer.observe(box, {
-    childList: true
-});
+// Создание функции-конструктора
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    };
+}
 
-// Отключение от отлслеживания 
-observer.disconnect();
+// Использование функции конструктора с использованием prototype 
+User.prototype.exit = function() {
+    console.log(`Пользователь ${this.name} ушел`)
+}
 
+// Добавление новых переменных для конструктора с использование оператора new
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
+
+//Вызов метода exit
+ivan.exit();
+
+// Вызов функции коснтруктора
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
