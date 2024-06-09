ab72b51b6df428c3df33b20249c66c0e0622818f
Vitalii <tather13@gmail.com>
2024-06-09 12:07:09 +0300

    HowCallThis

--- script.js
+++ script.js
@@ -1,33 +1,91 @@
 'use strict';
+// 1.
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this)
+//         return a + b;
+//     }
+//     console.log(sum());
+// }
+// showThis(4, 5);
+// 2.
+// const object = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         function shout() {
+//             console.log(this)
+//         }
+//         shout();
+//     }
+// }
+
+// object.sum();
+
+// 3.
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log("Hello! " + this.name);
+//     };
+// }
+// let ivan = new User('Ivan', 23);
+
+// function sayName(surname) {
+//     console.log(this);  
+//     console.log(this.name + surname)
+// }
+
+// const user = {
+//     name: 'John',
+// };
+
+// sayName.call(user, 'Smith');// Метод call вызывает функцию с указанным this и передает ей аргументы
+// sayName.apply(user, ['Smith']); // Похож на метод call, но есть одно важное отличие: apply принимает массив аргументов вместо списка.
+
+// function count(num) {
+//     return this*num;
+// }
+
+// const double = count.bind(2); // Метод bind используется для создания новой функции,которая при вызове будет иметь определенное значение this и, возможно,частично примененные аргумены
+// console.log(double(3));
+// console.log(double(13));
 
 
-// Функция конструктор
+// // 1) Обычная функция: this = window, но если use strict - undefined; 
+// // 2) Контекст у методов объекта - сам объект
+// // 3) this в конструкторах и классах - это новый экземпляр объекта
+// // 4) Ручная привязка this: call, apply, bind
 
-// Создание функции-конструктора
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function() {
-        console.log(`Hello ${this.name}`);
-    };
-}
+const btn = document.querySelector('button');
+btn.test = 1;
 
-// Использование функции конструктора с использованием prototype 
-User.prototype.exit = function() {
-    console.log(`Пользователь ${this.name} ушел`)
-}
+// btn.addEventListener('click', function() {
+//     this.style.backgroundColor = 'red';
+// });
 
-// Добавление новых переменных для конструктора с использование оператора new
-const ivan = new User('Ivan', 28);
-const alex = new User('Alex', 20);
+// const obj = {
+//     num: 5,
+//     sayNumber: function() {
+//         const say = () => {
+//             console.log(this.num);
+//         };
+//         say();
+//     }
+// }
 
-//Вызов метода exit
-ivan.exit();
+// obj.sayNumber();
 
-// Вызов функции коснтруктора
-ivan.hello();
-alex.hello();
+// const double = a => a * 2;
 
-console.log(ivan);
-console.log(alex);
+// console.log(double(4))
+
+btn.addEventListener('click', (event) => {
+console.log(event)
+});
+
+
+
