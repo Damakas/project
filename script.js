06de3759a953d5cf2cde19728ac730d330299e21
Vitalii <tather13@gmail.com>
2024-07-09 15:19:30 +0300

    this

--- script.js
+++ script.js
@@ -1,91 +1,89 @@
-'use strict';
-// 1.
+'use strict'
+//  1) 
+
 // function showThis(a, b) {
-//     console.log(this);
-//     function sum() {
-//         console.log(this)
-//         return a + b;
-//     }
-//     console.log(sum());
+// console.log(this);
+// function sum() {
+//   console.log(this)
+//   return a + b;
+//   }
+// console.log(sum());
 // }
-// showThis(4, 5);
-// 2.
-// const object = {
-//     a: 20,
-//     b: 15,
-//     sum: function() {
-//         function shout() {
-//             console.log(this)
-//         }
-//         shout();
-//     }
-// }
+// showThis(4,5);
 
-// object.sum();
+// 2)
 
-// 3.
-// function User(name, id) {
-//     this.name = name;
-//     this.id = id;
-//     this.human = true;
-//     this.hello = function() {
-//         console.log("Hello! " + this.name);
-//     };
+  // const obj = {
+// a : 20,
+// b : 15,
+// sum: function(){
+//      console.log(this)
+//     }
+// };
+// obj.sum();
+
+// 3)
+
+// function User(name,id) {
+//   this.name = name;
+//   this.id = id;
+//   this.human = true;
+//   this.hello = function() {
+//     console.log('Hello! ' + this.name)
+//   }
 // }
-// let ivan = new User('Ivan', 23);
+// let ivan = new User('Ivan', 23)
+
+// 4)
+
+// Методы функции-конструктора call(); apply(); bind()
 
 // function sayName(surname) {
-//     console.log(this);  
-//     console.log(this.name + surname)
+//   console.log(this);
+//   console.log(this.name + surname)
 // }
 
 // const user = {
-//     name: 'John',
-// };
+//   name : 'John'
+// }
 
-// sayName.call(user, 'Smith');// Метод call вызывает функцию с указанным this и передает ей аргументы
-// sayName.apply(user, ['Smith']); // Похож на метод call, но есть одно важное отличие: apply принимает массив аргументов вместо списка.
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
 
 // function count(num) {
-//     return this*num;
+//   return this*num;
 // }
 
-// const double = count.bind(2); // Метод bind используется для создания новой функции,которая при вызове будет иметь определенное значение this и, возможно,частично примененные аргумены
-// console.log(double(3));
-// console.log(double(13));
+// const double = count.bind(2);
+// console.log(double(3))
+// console.log(double(13))
 
 
-// // 1) Обычная функция: this = window, но если use strict - undefined; 
-// // 2) Контекст у методов объекта - сам объект
-// // 3) this в конструкторах и классах - это новый экземпляр объекта
-// // 4) Ручная привязка this: call, apply, bind
+
+
+// 1) Обычная функция : this = window,но если стоит use strict то будет равно undefined
+// 2) Контекст у методов объекта - сам объект
+// 3) this в конструкторах и классах - это новый экземпляр объекта
+// 4) Ручная привязка this: call,apply,bind
 
 const btn = document.querySelector('button');
-btn.test = 1;
 
-// btn.addEventListener('click', function() {
-//     this.style.backgroundColor = 'red';
-// });
-
-// const obj = {
-//     num: 5,
-//     sayNumber: function() {
-//         const say = () => {
-//             console.log(this.num);
-//         };
-//         say();
-//     }
-// }
-
-// obj.sayNumber();
-
-// const double = a => a * 2;
-
-// console.log(double(4))
-
-btn.addEventListener('click', (event) => {
-console.log(event)
+btn.addEventListener('click', function(){
+  this.style.backgroundColor = 'red';
 });
 
+const obj = {
+  num : 5,
+  sayNumber: function() {
+    const say = () => {
+      console.log(this.num);
+    };
+    say();
+  }
+};
 
+obj.sayNumber();
 
+const double = a => a * 2; //Сокращение стрелочной функции
+
+console.log(double(4))
