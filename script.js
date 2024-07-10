e71ff2aff05f0f45acf4977befcc2e45559ed300
Vitalii <tather13@gmail.com>
2024-07-10 11:28:58 +0300

    operatorRest

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 'use strict'
+// Оператор ...rest
 
+const log = function(a, b, ...rest) {
+    console.log(a, b, rest);
+}
 
+log('basic', 'rest', 'operator', 'usage');
 
+function calcOrDouble(number, basis = 2) {
+    console.log(number * basis);
+}
+
+calcOrDouble(3,5)
 
