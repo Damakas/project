b97d10d9a99ec83b0974c1565121f526cb1a44b8
Vitalii <tather13@gmail.com>
2024-07-12 10:45:06 +0300

    AJAX_serverCommunication

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
 'use strict'
 
-const persone = {
-    name: 'Alex',
-    tel: '+7444444',
-    parents: {
-        mom: 'Olga',
-        dad: 'Mike'
-    }
-};
-
-// console.log(JSON.stringify(persone)); //Метод который Отправляет данные  для бэкэнда
-// console.log(JSON.parse(JSON.stringify(persone))); // Метод который получает данные от бэкэнда
-
-//Как делается глубокое копирование 
-const clone = JSON.parse(JSON.stringify(persone));
-clone.parents.mom = 'Ann';
-console.log(persone);
-console.log(clone);
 
