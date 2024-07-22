fe5b206a0a154110a2af384d8627cf5449dabede
Vitalii <tather13@gmail.com>
2024-07-22 15:48:38 +0300

    encapsulationExample

--- script.js
+++ script.js
@@ -1,71 +1,49 @@
 'use strict'
 
-// Инкапсуляция в функциях
-
-// function User(name, age) {
-//     this.name = name;
-//     let userAge = age;
-
-//     this.say = function () {
-//         console.log(`Имя пользователя: ${this.name}, возраст ${userAge}`)
-//     }
-
-//     this.getAge = function () {
-//         return userAge;
-//     };
-
-//     this.setAge = function (age) {
-//         if (typeof age === 'number' && age > 0 && age < 110) {
-//             userAge = age;
-//         } else {
-//             console.log('Недопустимое значение')
-//         }
-//     }
-// }
-
-// const ivan = new User('Ivan', 27);
-// console.log(ivan.name);
-// console.log(ivan.getAge());
-
-
-// ivan.setAge(30);
-// ivan.setAge(300);
-// console.log(ivan.getAge());
-
-
-// ivan.say()
-
-// Инкапсуляция в классах
-
-class User {
-    constructor(name, age) {
-        this.name = name;
-        this._age = age;
+class BankAcc {
+    constructor(owner, balance) {
+        this._owner = owner;
+        this._balance = balance;
     }
 
-    #surname = 'Petrychenko'
-
-
-    say() {
-        console.log(`Имя пользователя: ${this.name}${this.#surname}, возраст ${this._age}`)
+    get owner() {
+        return this._owner
     }
 
-    get age() {
-        return this._age;
-    }
-
-    set age(age) {
-        if (typeof age === 'number' && age > 0 && age < 110) {
-            this._age = age;
+    set owner(name) {
+        if (name > 0) {
+            this._owner = name;
         } else {
-            console.log('Недопустимое значение')
+            console.log('Поле не может быть пустым');
+        }
+    }
+
+    get balance() {
+        return this._balance
+    }
+
+    deposit(amount) {
+        if (amount > 0) {
+            this._balance += amount;
+            console.log(`На ваш счет зачислено ${amount}`)
+        } else {
+            console.log('Неккоректно введена сумма')
+        }
+    }
+
+    windraw(amount) {
+        if (amount > 0 && amount <= this._balance) {
+            this._balance -= amount;
+            console.log(`С вамшего счета снято ${amount}`)
+        } else {
+            console.log('Недостаточно средств на счету')
         }
     }
 }
 
-const ivan = new User('Ivan', 27);
-console.log(ivan.surname)
-ivan.say()
-
-
+let newBalance = new BankAcc('Jora', 500);
+newBalance.deposit(500)
+newBalance.windraw(999)
+newBalance.windraw(300)
+console.log(newBalance)
 
