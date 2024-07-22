ddf0da80c14a260b9e2e17b23bab7315e3a9cb24
Vitalii <tather13@gmail.com>
2024-07-22 13:52:49 +0300

    GettersAndSetters

--- script.js
+++ script.js
@@ -1,44 +1,18 @@
 'use strict'
 
-// new RegExp('pattern', 'flags');
-// /pattern/flags
+const persone = {
+    name: 'Alex',
+    age: 25,
 
-// const ans = prompt('Введите ваше число');
+    get userAge() {
+        return this.age;
+    },
 
-// const reg = /\d/g;
-// console.log(ans.match(reg));
+    set userAge(num) {
+        this.age = num
+    }
+};
 
-const str = 'My name is R2D2';
-
-console.log(str.match(/\w\d\w\d/i)) // Выведет в консоль R2D2
-
-
-
-//Классы регулярных выражений
-
-// \d   Ищет все цифры
-// \w   Ищем все буквы или слова
-// \s   Ищет все пробелы
-
-// Обратные классы
-
-// D   Ищет не числа
-//W    Ищет не буквы
-//S     Ищет любой не пробельный символ
-
-
-
-//Флаги
-// i // Если мы хотим что-то найти вне зависимости от регистра
-// g // Когда пытаемся найти сразу несоклько вхождений
-// m // Включает в себя многострочный режим
-
-// console.log(ans.search(reg));
-// console.log(ans.match(reg));
-
-// const pass = prompt('Password');
-
-// console.log(pass.replace(/./g, '*')) //  Все символы введные в pass и появятся в консоли  в виде звездочек
-
-// console.log('12-32-56'.replace(/-/g, ':')); // заменяет дефис на двоеточие 12:32:56
+console.log(persone.userAge = 30);
+console.log(persone.userAge);
 
