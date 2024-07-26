6a745cb4775a56456462a82d1d612671ff8e78e9
Vitalii <tather13@gmail.com>
2024-07-26 14:07:07 +0300

    propertyDescriptorsFlags

--- additionallessons/script.js
+++ additionallessons/script.js
@@ -1,14 +1,37 @@
 'use strict';
 
-const boxesQuery = document.querySelectorAll('.box');
+// Аттрибуты(флаги) свойств объекта
+
+// writable        если будет true,то свойство можно изменить,если false ,то только для чтения
+// enumerable      если стоит true , то свойство будет перечислятся в циклах,если false то циклы будут его игнорировать
+// configurable    если стоит true,то свойство можно удалить,а аттрибуты можно изменить,если false,то нельзя
 
 
-//matches Иcпользуется для проверки по указанному Css-селектору
-boxesQuery.forEach(box => {
-    if (box.matches('.this')) console.log('This one!')
-});
+const user = {
+    name: 'Alex',
+    surname: 'Smith',
+    showMyPublicData: function () {
+        console.log(`${this.name} ${this.surname}`)
+    }
+}
+// Object.defineProperty(user, 'birthday', { value: prompt('Date?'), enumerable: true, configurable: true });// Метод который редактирует флаги свойства или создает новые свойства
+
+// console.log(Object.getOwnPropertyDescriptor(user, 'birthday'));//Метод который показывает флаги определенного свойства
+
+console.log(Object.getOwnPropertyDescriptor(Math, 'PI'));//Метод который показывает флаги определенного свойства
 
 
-//Метод ищет у своих родителей ближайший селектор по указаннмоу классу вверх по коду
-console.log(boxesQuery[0].closest('.wrapper'))
+Object.defineProperty(user, 'showMyPublicData', { enumerable: false })
+
+for (let key in user) console.log(key)
+
+Object.defineProperties(user, {
+    name: { writable: false },
+    surname: { writable: false }
+})
+
+// Object.defineProperty(user, 'name', { writable: false });// Метод который редактирует флаги свойства
+// Object.defineProperty(user, 'gender', { value: 'male' });// Метод позволяет создать новое свойство с указанным флагом,если не указать флаг,то они автоматически все будут false
+// console.log(Object.getOwnPropertyDescriptor(user, 'gender'));
+
 
